a245901ef142bf37ea9c9c56a6dd83dc6180ba7a
Kotvič <134196364+KotvicCodes@users.noreply.github.com>
2026-06-05 09:57:18 +0200

    add 'adding terms' logic
    - added a forloop which adds all coeficients of their respective power of the polynomial into a single number
    - changed a little bit how the spacing between different section is used

--- project/polynomial.py
+++ project/polynomial.py
@@ -4,6 +4,7 @@
 input = input("Input the polynomial to factorize: ")
 spaceless = input.replace(" ", "")
 
+
 #* Split terms
 terms = []
 termLenght = 0
@@ -19,6 +20,38 @@ for num, char in enumerate(spaceless):
     if (num + 1) == polynomialLenght:
         terms.append(spaceless[(num - termLenght + 1):num + 1])
 
+#* Discover variable 
+variable = ""
+
+for i in range(len(terms)):
+    for char in terms[i]:
+        if not char.isdigit():
+            variable = char
+            break
+
+
+#* Add terms
+powers = {}
+
+for term in terms:
+    # absolute & linear terms
+    if "^" not in term:
+        if variable in term:
+            term += "^1"
+        else:
+            term += f"{variable}^0"
+
+    # the rest of the logic
+    print(term)
+    coeficient = int(term.split(f"{variable}^")[0])
+    power = int(term.split(f"{variable}^")[1])
+
+    if power not in powers:
+        powers[power] = 0
+
+    powers[power] += coeficient
+
 
 print(f"user's polynomial: {spaceless}")
 print(f"terms in order: {", ".join(terms)}")
+print(f"dict of terms: {powers}")
